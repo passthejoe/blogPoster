422fdae9399f9189935440e07df076341b9d6c60
Steven Rosenberg <stevenhrosenberg@gmail.com>
2018-12-16 14:37:25 -0800

    Add explainers and instance variable @ping_needed to configuration example

--- blogPoster_configuration_example.rb
+++ blogPoster_configuration_example.rb
@@ -4,16 +4,21 @@
 @your_text_editor = "vim"
 
 # Twitter API
+
 @twitter_consumer_key        = "this_is_a_pretty_long_string_of_characters"
 @twitter_consumer_secret     = "this_is_an_even_longer_string_of_characters_for_the_Twitter_API"
 @twitter_access_token        = "this_is_yet_another_long_string_of_characters_for_the_Twitter_API"
 @twitter_access_token_secret = "just_another_long_string_of_characters_for_the_Twitter_API"
 
+# FTP account and server information
+
 @your_ftp_domain            = "ftp.YOUR_DOMAIN_HERE.com"
 @your_ftp_mode_passive      = true
-@your_ftp_login_name        = "your_FTP_server_login"
+@your_ftp_login_name        = "your_FTP_server_login_name"
 @your_ftp_password          = "your_FTP_server_password"
+
 @your_ftp_social_directory  = "/here/is/the/path/to/the/directory/where/your/social/posts/live"
 @your_ftp_other_directory   = "/here/is/the/path/to/another/directory/on/your/server/totally/optional"
 
+@ping_needed                = true
 @your_website_to_ping = "http://myblog.com/requires/calling/a/special/url/to/rebuild/the/index"

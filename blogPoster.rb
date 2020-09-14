77870d0a837145ecfcc8c10ad6e159ea5929a533
Steven Rosenberg <stevenhrosenberg@gmail.com>
2020-09-14 10:42:51 -0700

    Fixing search text for Ruby 2.7 detection

--- blogPoster.rb
+++ blogPoster.rb
@@ -650,7 +650,7 @@ menu = ["",
 		rescue
 			puts "\nSomething happened with Mastodon"
 			puts "Your toot did not go through"
-			if #{RUBY_VERSION}.include? "Ruby"
+			if #{RUBY_VERSION}.include? "2.7"
 				puts "... unless you are running Ruby 2.7,"
 				puts "and it is very likely your Toot"
 				puts "DID go through. Check your Mastodon"

adee9532b5662e780f406018d25bfc2c2ac5648d
Steven Rosenberg <stevenhrosenberg@gmail.com>
2020-09-14 10:41:11 -0700

    Refining Ruby 2.7 Mastodon message

--- blogPoster.rb
+++ blogPoster.rb
@@ -650,11 +650,11 @@ menu = ["",
 		rescue
 			puts "\nSomething happened with Mastodon"
 			puts "Your toot did not go through"
-			if #{RUBY_VERSION}.include? "2.7"
-				puts "... unless you are running Ruby 2.7,
-				and it is very likely your Toot
-				did go through. Check your Mastodon
-				instance to be sure"
+			if #{RUBY_VERSION}.include? "Ruby"
+				puts "... unless you are running Ruby 2.7,"
+				puts "and it is very likely your Toot"
+				puts "DID go through. Check your Mastodon"
+				puts "instance to be sure."
 			end
 		else
 			puts "Success!"
